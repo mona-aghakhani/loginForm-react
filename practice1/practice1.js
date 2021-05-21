f6b3cc97a1866e14252defc9b0195c6adcadbc1a
mona.akhani <mona.akhani2019@gmail.com>
2021-05-21 17:38:53 +0430

    practice1 finished

---
+++ practice1/practice1.js
@@ -0,0 +1,21 @@
+function myFunction() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    
+    // roye tamame radif haye jadval halghe mizanim va un tr hayi k ba kalame ei k serach shode match nabashan hazf mishe
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
